9580e56f8c334625e18a6d25447762e7b820bbd4
yaramalakeerthi2006-cloud <yaramalakeerthi2006@gmail.com>
2026-08-15 19:47:10 +0530

    Implement function to count identical pairs

---
+++ Number of Good Pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        n=len(nums)
+        count=0
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]==nums[j]:
+                    count+=1
+                
+        return count      
